06cd2894a93f03557e90b2a8eb4c6ce875ec871b
Wendy-TW <132725451+Wendy-TW@users.noreply.github.com>
2023-05-27 06:18:11 +0000

    commit

--- Lesson_8_1.py
+++ Lesson_8_1.py
@@ -9,7 +9,18 @@ if response.ok:
    
 if response.status_code==200:
     print("connect successfully")
-    all_text=response.text
-    print(type(all_text))
+    all_data=response.json()
+    print(type(all_data))
 else:  print(f"fail code:{response.status_code}")
+
+import pandas as pd
+#class pandas.DataFrame(data=None, index=None, columns=None, dtype=None, copy=None)[source]
+DF1= pd.DataFrame(data=all_data)
+
+Series1=DF1['sna']
+mask=DF1['sbi']<=3
+Mask_DF=DF1[mask]
+Mask_DF.to_csv('可借用車輛小於3站點.csv')
+
+Mask_DF.to_excel('可借用車輛小於3站點.xlsx')  #pip install openpyxl
 
