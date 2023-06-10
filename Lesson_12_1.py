80406cfe7b5d6eb6cfb5b86e5157ea9ae7a78c04
Wendy-TW <132725451+Wendy-TW@users.noreply.github.com>
2023-06-10 07:53:36 +0000

    commit

--- Lesson_12_1.py
+++ Lesson_12_1.py
@@ -10,17 +10,16 @@ codeSeries=codeFrame['證券代號'].astype(str) + codeFrame['證券名稱']
 with st.sidebar:
     selected_codes = st.multiselect("請選擇股票:",codeSeries,
                                     max_selections=4)
+ 
 @st.cache_data
 def fetch_stock_dataFrame(id):
     stock_dataFrame = yf.download(id,start='2022-01-01')
     return stock_dataFrame
- 
 
 for code in selected_codes:
-    code1=code[:4]+".TW"
-    code_stock_dataFrame=fetch_stock_dataframe(code1)
-    code_stock_DF_sorted=code_stock_dataFrame.sort_index(ascending=false)
+    code1 = code[:4]+'.TW'
+    code_stock_dataFrame = fetch_stock_dataFrame(code1)
+    code_stock_dataFrame_sorted = code_stock_dataFrame.sort_index(ascending=False)
     st.subheader(code)
-    st.dataframe(code_stock_DF_sorted, width=1024)
+    st.dataframe(code_stock_dataFrame_sorted,width=1024)
     st.divider()
-    st.write(code)
